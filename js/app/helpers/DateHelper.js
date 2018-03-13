24dc68c3e8a16d81995298558560ecfa45a80dd5
Gustavo <Gustavo Scherer>
2018-03-13 02:06:27 -0400

    Criado um helper para ajudar nas conversões de data para texto e vice-versa, e criado um modelo de Lista de Negociacoes, e feitas as devidas alterações no index.html e no NegocioController

---
+++ js/app/helpers/DateHelper.js
@@ -0,0 +1,21 @@
+class DateHelper {
+
+    constructor() {
+        throw new Error('DateHelper não pode ser instanciada');
+    }
+
+    static dataParaTexto(data) {
+        // return data.getDate()
+        //     + '/' + (data.getMonth() + 1)
+        //     + '/' +  data.getFullYear();
+        return `${data.getDate()}/${data.getMonth() + 1}/${data.getFullYear()}`; // Template string mesma coisa
+                                                                                // que o de cima
+    }
+
+    static textoParaData(texto) {
+        if(!/\d{4}-\d{2}-\d{2}/.test(texto))
+            throw new Error('Deve estar no formato aaaa-mm-dd');
+
+        return new Date(...texto.split('-').map((item, indice) => item - indice % 2));
+    }
+}
